da10248a8a44f999551e692826b6fd753cd58bbd
Nathan Yan <nathan_yan_alves@hotmail.com>
2026-09-23 14:54:37 -0300

    feat(main): configure logging output

--- src/diario_aberto/main.py
+++ src/diario_aberto/main.py
@@ -1,4 +1,12 @@
+import logging
+
 from diario_aberto.scraper.editions import EditionScraper
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s %(levelname)s %(name)s: %(message)s",
+    datefmt="%H:%M:%S",
+)
 
 scraper = EditionScraper()
 
